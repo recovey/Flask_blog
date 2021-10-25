4ddf7a6e4484efb4e59915c496c05393f0d748ad
think <2540368640@qq.com>
2021-10-25 13:43:24 +0800

    第七次上传-个人资料1

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from hashlib import md5
+
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
@@ -12,11 +14,18 @@ def load_user(id):
 
 
 class User(UserMixin, db.Model):
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
     def __repr__(self):
         # return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,7 @@ from flask_login import login_required
 from werkzeug.urls import url_parse
 from app import db
 from app.forms import RegistrationForm
+from datetime import datetime
 
 
 # 2个路由
@@ -76,3 +77,21 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author':user, 'body':'Test post #1'},
+        {'author':user, 'body':'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request  # 访问任意一个函数都会执行这里
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
