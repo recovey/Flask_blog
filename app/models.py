3cd08fafbfbfade92d78a2949ad760f61fc73434
think <2540368640@qq.com>
2021-10-26 16:15:15 +0800

    第八次上传-用户关注

--- app/models.py
+++ app/models.py
@@ -4,13 +4,21 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
 from datetime import datetime
-from flask_login import UserMixin
 from app import login
+from flask_login import UserMixin
 
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
+# 关注者关联表
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 
 class User(UserMixin, db.Model):
@@ -27,6 +35,34 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id).order_by(
+            Post.timestamp.desc())
+
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
+
     def __repr__(self):
         # return '<User {}>'.format(self.username)
         return '<User {}, Email {}, Password_Hash {}, Posts {}'.format(self.username, self.email, self.password_hash,
@@ -38,8 +74,18 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    # 已关注用户的帖子的查询
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
-class Post(db.Model): # UserMixin,
+    def followed_posts(self):
+        followed = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
+
+class Post(db.Model):  # UserMixin,
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -47,3 +93,6 @@ class Post(db.Model): # UserMixin,
 
     def __repr__(self):
         return '<Post {}'.format(self.body)
+
+
+
