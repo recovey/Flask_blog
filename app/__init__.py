68bef0545e62017e3e30070595c081eb290a4fda
think <2540368640@qq.com>
2021-10-28 11:15:51 +0800

    第十一次上传-Bootstrap美化站点

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy  # 从包中导入类
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)  # 将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 app.config.from_object(Config)
@@ -13,5 +14,6 @@ print('等会谁（哪个包或模块）在使用我：', __name__)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models  # 从app包中导入模块routes
