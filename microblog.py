3ad507ac5e18ace6df630a72e824da863f41f37e
think <2540368640@qq.com>
2021-10-29 17:26:33 +0800

    第十四次上传-国际化和本地化2

--- microblog.py
+++ microblog.py
@@ -1,9 +1,13 @@
 from app import app, db
 from app.models import User, Post
-if __name__ == '__main__':
-    app.run(debug=True)
+from app import cli
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
